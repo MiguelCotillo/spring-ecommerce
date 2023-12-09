7bf953958e19fff30c2a7c3aa09e1085ddf7b1fe
Miguel <pcotillo7@gmail.com>
2023-12-09 16:42:42 -0500

    Funcionalidad de Actualizar un producto

--- src/main/java/com/ecommerce/migdev/app/controller/ProductoController.java
+++ src/main/java/com/ecommerce/migdev/app/controller/ProductoController.java
@@ -1,11 +1,14 @@
 package com.ecommerce.migdev.app.controller;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -42,5 +45,21 @@ public class ProductoController {
 		return "redirect:/productos";
 	}
 	
+	@GetMapping("/edit/{id}")
+	public String editar(@PathVariable Integer id, Model model) {
+		Producto producto = new Producto();
+		Optional<Producto> opProducto = serv.Obtener(id);
+		producto = opProducto.get();
+		LOGGER.info("Producto Guardado {} ", producto);
+		model.addAttribute("producto", producto);
+		
+		return "productos/edit";
+	}
+	
+	@PostMapping("/actualizar")
+	public String actualizar(Producto producto) {
+		serv.editar(producto);
+		return "redirect:/productos";
+	}
 	
 }
