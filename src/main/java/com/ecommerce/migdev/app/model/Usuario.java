578e7392d886deb2fec15a2823849321177cd7dc
Miguel <pcotillo7@gmail.com>
2023-12-08 23:09:45 -0500

    Relacionando tablas

--- src/main/java/com/ecommerce/migdev/app/model/Usuario.java
+++ src/main/java/com/ecommerce/migdev/app/model/Usuario.java
@@ -1,6 +1,20 @@
 package com.ecommerce.migdev.app.model;
 
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
 public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nombre;
 	private String username;
@@ -9,12 +23,18 @@ public class Usuario {
 	private String telefono;
 	private String tipo;
 	private String password;
+	
+	@OneToMany(mappedBy = "usuario")
+	private List<Producto> productos;
+	
+	@OneToMany(mappedBy = "usuario")
+	private List<Orden> ordenes;
 
 	public Usuario() {
 	}
 
 	public Usuario(Integer id, String nombre, String username, String email, String direccion, String telefono,
-			String tipo, String password) {
+			String tipo, String password, List<Producto> productos) {
 		super();
 		this.id = id;
 		this.nombre = nombre;
@@ -24,6 +44,7 @@ public class Usuario {
 		this.telefono = telefono;
 		this.tipo = tipo;
 		this.password = password;
+		this.productos = productos;
 	}
 
 	public Integer getId() {
@@ -88,6 +109,22 @@ public class Usuario {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	
+	public List<Producto> getProductos() {
+		return productos;
+	}
+
+	public void setProductos(List<Producto> productos) {
+		this.productos = productos;
+	}
+
+	public List<Orden> getOrdenes() {
+		return ordenes;
+	}
+
+	public void setOrdenes(List<Orden> ordenes) {
+		this.ordenes = ordenes;
 	}
 
 	@Override
