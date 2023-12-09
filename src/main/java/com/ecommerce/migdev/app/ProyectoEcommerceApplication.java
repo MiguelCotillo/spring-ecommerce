58be1b8090058d185fa294eaf4d106e510404404
Miguel <pcotillo7@gmail.com>
2023-12-08 20:30:51 -0500

    Creacion de Estructura del proyecto y clases Modal

---
+++ src/main/java/com/ecommerce/migdev/app/ProyectoEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.migdev.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoEcommerceApplication.class, args);
+	}
+
+}
